9dbdfd0fc6d1a7d1897a59c0403fc21e9347200c
Mohamed <57219435+Posedion1920@users.noreply.github.com>
2025-05-12 09:47:57 +0200

    gavepakker mobile version

--- pages/gavekort-og-ovrige-billetter.vue
+++ pages/gavekort-og-ovrige-billetter.vue
@@ -6,8 +6,38 @@ import Header from '@/components/Header.vue';
 <template>
   <Header />
   <main>
-    <img src="../assets/img/Sauna-2025.hd (1).jpg" alt="">
-
+    <section class="udvalg">
+      <article>
+        <div class="udvalgCard">
+          <img src="../assets/img/GavePakkeLogo.png" alt="" class="logo">
+          <img src="../assets/img/GavepakkeIkon.png" alt="" class="iconthing">
+          <img src="../assets/img/Gavepakke1Mobil.png" alt="">
+        </div>
+        <div class="btn">
+          <button>Køb</button>
+        </div>
+      </article>
+       <article>
+        <div class="udvalgCard">
+          <img src="../assets/img/GavePakkeLogo.png" alt="" class="logo">
+          <img src="../assets/img/GavepakkeIkon.png" alt="" class="iconthing">
+          <img src="../assets/img/Gavepakke1Mobil.png" alt="">
+        </div>
+        <div class="btn">
+          <button>Køb</button>
+        </div>
+      </article>
+       <article>
+        <div class="udvalgCard">
+          <img src="../assets/img/GavePakkeLogo.png" alt="" class="logo">
+          <img src="../assets/img/GavepakkeIkon.png" alt="" class="iconthing">
+          <img src="../assets/img/Gavepakke1Mobil.png" alt="">
+        </div>
+        <div class="btn">
+          <button>Køb</button>
+        </div>
+      </article>
+    </section>
   </main>
   <Footer />
 
@@ -15,5 +45,55 @@ import Header from '@/components/Header.vue';
 
 
 <style scoped>
+main{
+  padding: 0px 12px;
+}
+
+.udvalg{
+  display: grid;
+  row-gap: 6rem;
+}
+
+.udvalgCard{
+  background-color: white;
+  box-shadow: 0px 0px 20px 2px #4C90FF;
+  border-radius: 30px;
+  padding: 56px 0px 16px 0px;
+  position: relative;
+}
+.udvalgCard img:last-of-type{
+  border-radius: 30px;
+  display: block;
+  margin: auto;
+}
+
+.iconthing{
+  position: absolute;
+  top: 10px;
+  left: 50%;
+  transform: translateX(-50%);
+}
+
+.logo{
+  position: absolute;
+  top: 10px;
+  left: 35px;
+}
+
+.btn{
+  margin-top: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.btn button{
+  color: white;
+  border: none;
+  background-color: var(--interactive-red);
+  font-family: var(--font-headings);
+  border-radius: 5px;
+  padding: 12px 32px;
+}
 
 </style>
+
