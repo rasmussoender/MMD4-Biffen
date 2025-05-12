b564613644274a26f5f29cfdc60917a96bcf0b46
Mohamed <57219435+Posedion1920@users.noreply.github.com>
2025-05-12 11:01:53 +0200

    små justeringer

--- pages/gavekort-og-ovrige-billetter.vue
+++ pages/gavekort-og-ovrige-billetter.vue
@@ -11,7 +11,7 @@ import Header from '@/components/Header.vue';
         <div class="udvalgCard">
           <img src="../assets/img/GavePakkeLogo.png" alt="" class="logo">
           <img src="../assets/img/GavepakkeIkon.png" alt="" class="iconthing">
-          <img src="../assets/img/Gavepakke1Mobil.png" alt="">
+          <img src="../assets/img/Gavepakke1.png" alt="">
         </div>
         <div class="btn">
           <button>Køb</button>
@@ -21,7 +21,7 @@ import Header from '@/components/Header.vue';
         <div class="udvalgCard">
           <img src="../assets/img/GavePakkeLogo.png" alt="" class="logo">
           <img src="../assets/img/GavepakkeIkon.png" alt="" class="iconthing">
-          <img src="../assets/img/Gavepakke1Mobil.png" alt="">
+          <img src="../assets/img/Gavepakke2.png" alt="">
         </div>
         <div class="btn">
           <button>Køb</button>
@@ -31,7 +31,25 @@ import Header from '@/components/Header.vue';
         <div class="udvalgCard">
           <img src="../assets/img/GavePakkeLogo.png" alt="" class="logo">
           <img src="../assets/img/GavepakkeIkon.png" alt="" class="iconthing">
-          <img src="../assets/img/Gavepakke1Mobil.png" alt="">
+           <img src="../assets/img/Gavepakke3.png" alt="">
+        </div>
+        <div class="btn">
+          <button>Køb</button>
+        </div>
+      </article>
+       <article>
+        <div class="udvalgCard">
+          <img src="../assets/img/GavePakkeLogo.png" alt="" class="logo">
+          <img src="../assets/img/GavepakkeIkon.png" alt="" class="iconthing">
+          <div class="gavepakke4img">
+          <div class="textOverlay">
+            <p>Overrask en du holder af</p>
+            <div class="redline"></div>
+            <p>- med en personlig gave leveret af Gavebuddet – hurtigt, nemt og uforglemmeligt.  </p>
+          </div>
+          <img src="../assets/img/Gavepakke4.png" alt="">
+          <div class="overlay"></div>
+          </div>
         </div>
         <div class="btn">
           <button>Køb</button>
@@ -51,6 +69,7 @@ main{
 
 .udvalg{
   display: grid;
+  justify-items: center;
   row-gap: 6rem;
 }
 
@@ -58,13 +77,13 @@ main{
   background-color: white;
   box-shadow: 0px 0px 20px 2px #4C90FF;
   border-radius: 30px;
-  padding: 56px 0px 16px 0px;
+  padding: 56px 16px 16px 16px;
   position: relative;
+  display: inline-block;
+
 }
 .udvalgCard img:last-of-type{
   border-radius: 30px;
-  display: block;
-  margin: auto;
 }
 
 .iconthing{
@@ -86,6 +105,7 @@ main{
   justify-content: center;
   align-items: center;
 }
+
 .btn button{
   color: white;
   border: none;
@@ -93,6 +113,52 @@ main{
   font-family: var(--font-headings);
   border-radius: 5px;
   padding: 12px 32px;
+}
+
+.gavepakke4img{
+  position: relative;
+}
+.overlay{
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.69);
+  top: 0px;
+  border-radius: 30px;
+}
+.textOverlay{
+  margin-top: 64px;
+  padding: 0px 16px;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  z-index: 100;
+}
+.redline{
+  width: 40%;
+  height: 2px;
+  background-color: var(--interactive-red);
+}
+.textOverlay p:last-of-type{
+  text-align: center;
+  font-size: 14px;
+  
+}
+
+@media (min-width: 1000px){
+   main{
+    padding: 0 var(--space-container);
+  }
+  .udvalg{
+    grid-template-columns: repeat(3,1fr);
+    justify-items: center;
+    row-gap: 8rem;
+  }
+  .udvalg article:last-of-type{
+    grid-column: 2;
+  }
+  
 }
 
 </style>
