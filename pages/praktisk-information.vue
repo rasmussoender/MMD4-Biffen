bb4e52dd53a300049f9fef1e4f718ad0295acce7
Mohamed <57219435+Posedion1920@users.noreply.github.com>
2025-05-12 20:14:05 +0200

    gået i gang med praktisk info

--- pages/praktisk-information.vue
+++ pages/praktisk-information.vue
@@ -5,13 +5,36 @@
 <template>
   <Header />
   <main>
-    <img src="../assets/img/Sauna-2025.hd (1).jpg" alt="">
-
+    <section>
+      <div class="kontakt">
+        <div>
+          <div class="navn">
+            <div><label for="fnavn">Fornavn<span>*</span></label></div>
+            <div><input type="text" name="fornavn" id="fnavn"></div>
+          </div>
+          <div class="efternavn">
+           <div> <label for="enavn">Efternavn<span>*</span></label></div>
+            <div><input type="text" name="efternavn" id="enavn"></div>
+          </div>
+        </div>
+      </div>
+    </section>
   </main>
   <Footer />
 
 </template>
 
 <style scoped>
+.kontakt{
+  background-color: var(--secondary-blue);
+  padding: 25px 25px;
+}
+.kontakt div:first-of-type{
+  display: flex;
+  justify-content: space-between;
+}
+main{
+  padding: 0px 12px;
+}
 
 </style>
