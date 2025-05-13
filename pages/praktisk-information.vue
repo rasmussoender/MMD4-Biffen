6bac64edb1221bb468af18ddac89d18f0f71cf12
Mohamed <57219435+Posedion1920@users.noreply.github.com>
2025-05-13 11:06:50 +0200

    contact form

--- pages/praktisk-information.vue
+++ pages/praktisk-information.vue
@@ -6,15 +6,48 @@
   <Header />
   <main>
     <section>
-      <div class="kontakt">
-        <div>
-          <div class="navn">
-            <div><label for="fnavn">Fornavn<span>*</span></label></div>
-            <div><input type="text" name="fornavn" id="fnavn"></div>
+        <div class="contact">
+        <div class="wrapper">
+        <div class="firstname">
+          <div>
+            <label for="fname">Fornavn</label>
           </div>
-          <div class="efternavn">
-           <div> <label for="enavn">Efternavn<span>*</span></label></div>
-            <div><input type="text" name="efternavn" id="enavn"></div>
+          <div>
+            <input type="text" name="fname" id="fname">
+          </div>
+        </div>
+
+        <div class="lastname">
+          <div>
+            <label for="fname">Efternavn</label>
+          </div>
+          <div>
+            <input type="text" name="fname" id="fname">
+          </div>
+        </div>
+        </div>
+        <div class="email">
+          <div>
+          <label for="email">E-mail</label>
+          </div>
+          <div>
+            <input type="email" name="email" id="email">
+          </div>
+        </div>
+          <div class="phone">
+          <div>
+          <label for="phone">Telefon</label>
+          </div>
+          <div>
+            <input type="tel" name="phone" id="phone">
+          </div>
+        </div>
+        <div class="message">
+          <div>
+            <label for="message">Besked</label>
+          </div>
+          <div>
+            <textarea id="message" rows="9"></textarea>
           </div>
         </div>
       </div>
@@ -23,18 +56,34 @@
   <Footer />
 
 </template>
-
 <style scoped>
-.kontakt{
+textarea{
+  width: 100%;
+  resize: none;
+}
+.wrapper{
+  display: flex;
+
+  gap: 1rem;
+}
+
+input{
+  width: 100%;
+  box-sizing: border-box;
+}
+
+.firstname, .lastname{
+  display: flex;
+  flex-direction: column;
+}
+
+.contact{
   background-color: var(--secondary-blue);
   padding: 25px 25px;
+
 }
-.kontakt div:first-of-type{
-  display: flex;
-  justify-content: space-between;
-}
+
 main{
   padding: 0px 12px;
 }
-
 </style>
