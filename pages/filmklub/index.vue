dd3c809c92ffe99737085f84e52f135e4e171428
nicholaslauiii <nicholaslaustsen@gmail.com>
2025-05-15 00:10:42 +0200

    +

--- pages/filmklub/index.vue
+++ pages/filmklub/index.vue
@@ -2,7 +2,45 @@
 
 </script>
 <template>
-
+<section class="forside-entry-section">
+  <div class="forside-entry-grid">
+    <div class="forside-entry-card forside-entry-card--white-bg">
+      <img src="../assets/img/cinemateket-entry.png" alt="Card 1">
+      <hr>
+      <h3>Cinemateket</h3>
+      <p>Oplev store klassikere, sjældne filmperler og spændende events i Cinemateket i Biffen.</p>
+    </div>
+    <div class="forside-entry-card">
+      <img src="../assets/img/filmklubben-entry.png" alt="Card 2">
+      <hr>
+      <h3>Filmklubber</h3>
+      <p>Meld dig ind i en filmklub, og se udvalgte film til reduceret pris!</p>
+    </div>
+    <div class="forside-entry-card">
+      <img src="../assets/img/events-entry.png" alt="Card 3">
+      <hr>
+      <h3>Events</h3>
+      <p>Biffen arrangerer året igennem en lang række spændende events - altid med den gode film i centrum.</p>
+    </div>
+    <div class="forside-entry-card">
+      <img src="../assets/img/gavekort-entry.png" alt="Card 4">
+      <hr>
+      <h3>Gavekort</h3>
+      <p>Et gavekort til Biffen er mere end bare en gave – det er en oplevelse. Se også vores øvrige billetter.</p>
+    </div>
+    <div class="forside-entry-card">
+      <img src="../assets/img/a-salen_lille.jpg" alt="Card 5">
+      <hr>
+      <h3>Book en biografsal</h3>
+      <p>Book en af vores sale. Perfekt til skoler, virksomhedsarrangementer, børnefødselsdage mm.</p>
+    </div>
+    <div class="forside-entry-card">
+      <img src="../assets/img/kontakt-entry.jpg" alt="Card 6">
+      <hr>
+      <h3>Kontakt os</h3>
+    </div>
+  </div>
+</section>
 </template>
 <style scoped>
 
