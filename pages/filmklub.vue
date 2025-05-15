6882f301a9a532fdf55c0d3f4cc077ac3e4435da
nicholaslauiii <nicholaslaustsen@gmail.com>
2025-05-15 14:08:02 +0200

    +

---
+++ pages/filmklub.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Header from '@/components/Header.vue';
+
+</script>
+
+<template>
+  <Header />
+  <main>
+
+  </main>
+  <Footer />
+
+</template>
+
+<style scoped>
+
+</style>
